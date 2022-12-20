af102851e3407cc90952e5986bcfd90502883a89
marrwong <36671194+marrwong@users.noreply.github.com>
2022-12-21 02:32:19 +0800

    Prettify files
    -Format App.tsx and main.tsx based on prettier config

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,27 @@
-import { useState } from 'react'
-import './App.scss'
-import { Button } from 'react-bootstrap'
+import { useState } from 'react';
+import './App.scss';
+import { Button } from 'react-bootstrap';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Vite + React + Typescript</h1>
       <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test Hot Module Reload
         </p>
       </div>
       <div>
-        <p className="tools-used">
-          Tools used:
-        </p>
+        <p className="tools-used">Tools used:</p>
         <p>
           ReactJS / Vite / TypeScript / Jest / Testing Library / ESLint / SASS / Bootstrap / React-bootstrap / Axios
         </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
